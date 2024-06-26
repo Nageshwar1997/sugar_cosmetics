b9268a7f2f7f18e524b71c670de9f93f6ecdf01c
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-26 19:46:04 +0530

    Start to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Context from "./contexts";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Context.Provider value={{}}>
+        <ToastContainer position="top-center" autoClose={2000} />
+        <Header />
+        <main className="">
+          <Outlet />
+        </main>
+        <Footer />
+      </Context.Provider>
+    </>
   );
 }
 
